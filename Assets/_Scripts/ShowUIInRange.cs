065cdd13ad4c2c3075e042569c1183b540570df1
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-04-07 22:53:00 -0400

    playermovement / OnTriggerDisplay

---
+++ Assets/_Scripts/ShowUIInRange.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUIInRange : MonoBehaviour
+{
+    public GameObject ui;
+
+    void Start()
+    {
+        HideUI();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ShowUI();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            HideUI();
+        }
+    }
+
+    public void HideUI()
+    {
+        ui.SetActive(false);
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+    }
+}
---
+++ Assets/_Scripts/movement, camera/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    public float walkspeed = 4f;
+    public float runspeed = 12f;
+    public CharacterController cc;
+
+    public bool isRunning;
+    public bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        cc.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+            speed = runspeed;
+        }
+        else
+        {
+            isRunning = false;
+            speed = walkspeed;
+        }
+
+    }
+}
